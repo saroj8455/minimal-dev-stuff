fafe013c89d90fff6d9c9d46a78022e2971d2fd3
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2022-03-24 22:53:19 +0530

    minimal container setup bootstrap_4 Impl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import * as saveAs from 'file-saver';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'minimal-dev-stuff';
+  pdfEndPoint = 'assets/CESCO_Detailed_Notification_English-Odia.pdf';
+  constructor(private readonly http: HttpClient) {}
+  downloadFile() {
+    this.http
+      .get(this.pdfEndPoint, {
+        responseType: 'blob',
+        headers: { Accept: 'application/pdf' },
+      })
+      .subscribe((blob) => {
+        saveAs(blob, 'download.pdf');
+      });
+  }
 }
